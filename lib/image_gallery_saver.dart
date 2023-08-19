b6c9f4129ee1ce066074c3585fa2ce54d18c1d0b
Salim Murshed <33266090+salimmurshed@users.noreply.github.com>
2023-08-19 17:04:41 +0600

    PLACE ADDED

--- lib/image_gallery_saver.dart
+++ lib/image_gallery_saver.dart
@@ -10,7 +10,7 @@ class ImageGallerySaver {
   /// imageBytes can't null
   /// return Map type
   /// for example:{"isSuccess":true, "filePath":String?}
-  static FutureOr<dynamic> saveImage(Uint8List imageBytes,
+  static FutureOr<dynamic> saveImage(Uint8List imageBytes,String place,
       {int quality = 80,
       String? name,
       bool isReturnImagePathOfIOS = false}) async {
@@ -19,18 +19,20 @@ class ImageGallerySaver {
       'imageBytes': imageBytes,
       'quality': quality,
       'name': name,
+      'place': place,
       'isReturnImagePathOfIOS': isReturnImagePathOfIOS
     });
     return result;
   }
 
   /// Save the PNG，JPG，JPEG image or video located at [file] to the local device media gallery.
-  static Future saveFile(String file,
+  static Future saveFile(String file,String place,
       {String? name, bool isReturnPathOfIOS = false}) async {
     final result = await _channel.invokeMethod(
         'saveFileToGallery', <String, dynamic>{
       'file': file,
       'name': name,
+      'place': place,
       'isReturnPathOfIOS': isReturnPathOfIOS
     });
     return result;
